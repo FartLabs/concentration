f0e659a92b7e001669659c5baa99b836f26c8460
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-02 14:32:32 -0700

    add sample concentration game logic
    Adapted source code from: <https://github.com/joysofcode/svelte-matching-game/blob/cc050f6b404f05da549a5c10be93bb48c828d752/src/routes/%2Bpage.svelte>.

--- src/routes/[amount=even]/+page.svelte
+++ src/routes/[amount=even]/+page.svelte
@@ -2,6 +2,7 @@
 	import type { PageData } from './$types';
 	// import Sound from '$lib/components/sound.svelte';
 	// import { makeSoundURL } from '$lib/sounds';
+	import Concentration from '$lib/components/concentration.svelte';
 
 	export let data: PageData;
 </script>
@@ -9,3 +10,10 @@
 <!-- Concentration game  -->
 
 {@debug data}
+
+<Concentration
+	values={data.sounds.map(({ sound }) => {
+		return sound;
+		// return sound.replace()
+	})}
+/>
